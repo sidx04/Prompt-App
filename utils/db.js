6038cb3dc068516c03709415e7320583f3bea2b1
Onion-Man <siddpal04@gmail.com>
2023-06-28 17:22:07 +0530

    database connection and database schema

---
+++ utils/db.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+var isConnected = false;
+
+export const connectDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("database is connected...");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "promptshare",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("database is connected...");
+  } catch (error) {
+    console.log({ msg: error.message });
+  }
+};
