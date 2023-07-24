fc6a6cad7f5a002fe43769849e576d19ba496651
sidx04 <siddpal04@gmail.com>
2023-07-25 01:07:47 +0530

    refactor(dk what this is for)

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,14 @@
 import Prompt from "@models/Prompt";
 import { connectDB } from "@utils/db";
 
-export const GET = async (req) => {
+export const GET = async (request) => {
   try {
     await connectDB();
+
     const prompts = await Prompt.find({}).populate("creator");
 
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
-    console.log(error);
-    return new Response("Failed to fetch all prompt posts...", { status: 500 });
+    return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
